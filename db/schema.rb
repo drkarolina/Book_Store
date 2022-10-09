1c556609bc4eed17533101c0ee5d63df67b1ee60
drkarolina <k.drahanchuk@gmail.com>
2022-10-09 18:40:11 +0300

    Add category model

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,15 @@
-ActiveRecord::Schema.define(version: 2022_10_07_165135) do
-
+ActiveRecord::Schema.define(version: 2022_10_09_152153) do
   enable_extension "plpgsql"
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
