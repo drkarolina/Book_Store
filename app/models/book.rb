9b79b0191dd0b23a2e4346d476644312ed884e20
drkarolina <k.drahanchuk@gmail.com>
2022-10-09 19:13:49 +0300

    Add author-book table

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
+  has_many :author_book, dependent: :destroy
+  has_many :authors, through: :author_book, dependent: :destroy
+
   belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_09_154042) do
+ActiveRecord::Schema.define(version: 2022_10_09_161008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "author_books", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_author_books_on_author_id"
+    t.index ["book_id"], name: "index_author_books_on_book_id"
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -43,5 +52,7 @@ ActiveRecord::Schema.define(version: 2022_10_09_154042) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "author_books", "authors"
+  add_foreign_key "author_books", "books"
   add_foreign_key "books", "categories"
 end
