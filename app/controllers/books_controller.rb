9eb48ad7ba54e05639df2a7e8245168c9a5eec5b
drkarolina <k.drahanchuk@gmail.com>
2022-10-12 19:53:13 +0300

    Delete include(:authors)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,13 @@ class BooksController < ApplicationController
   BOOKS_LOAD_NUMBER = 8
 
   def index
-    books = Book.includes(:authors).all.decorate
+    books = Book.all.decorate
     @collection = BooksQuery.new(books, params[:category_id], params[:sort_by]).filter_and_sort
     @books = @collection.limit(current_books_count)
   end
 
   def show
-    @book = Book.includes(:authors).find(params[:id]).decorate
+    @book = Book.find(params[:id]).decorate
   end
 
   def current_books_count
