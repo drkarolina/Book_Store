7602bd47cff3338b7818cd40579aade739980760
drkarolina <k.drahanchuk@gmail.com>
2022-10-17 18:09:07 +0300

    Fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,8 @@ class User < ApplicationRecord
   validates :password, format: { with: Constants::PASSWORD_FORMAT }
 
   def self.from_omniauth(auth)
-    name_split = auth.info.name.split
     user = User.where(email: auth.info.email).first
-    user || User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[0], first_name: name_split[1],
+    user || User.create!(provider: auth.provider, uid: auth.uid,
                          email: auth.info.email, password: Devise.friendly_token[0, 20])
   end
 end
