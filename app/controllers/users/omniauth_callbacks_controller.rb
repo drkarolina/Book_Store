d253cd18c1e251d66ddf059f6ce3413000fef60c
drkarolina <k.drahanchuk@gmail.com>
2022-10-17 18:27:57 +0300

    Fix user model

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,17 +3,23 @@ module Users
     def facebook
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-      else
-        session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url
-      end
+      @user.persisted? ? authenticate_user : registrate_user
     end
 
     def failure
       redirect_to root_path
+    end
+
+    private
+
+    def registrate_user
+      session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_url
+    end
+
+    def authenticate_user
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     end
   end
 end
