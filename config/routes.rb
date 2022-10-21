27a398d3a025109f0bb2995902d4fd700eae1079
drkarolina <k.drahanchuk@gmail.com>
2022-10-21 18:16:30 +0300

    Add settings controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "pages#index"
   resources :books, only: %i[index show]
+  resources :settings, only: %i[index]
 end
