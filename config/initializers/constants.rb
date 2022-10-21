278b31ef6c53638aa16863e09e422f0b6cad5aea
drkarolina <k.drahanchuk@gmail.com>
2022-10-21 15:52:33 +0300

    Add address form

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -10,4 +10,15 @@ module Constants
   }.freeze
   DEFAULT_SORT = SORT_BY.keys.first
   PASSWORD_FORMAT = /\A(?!.*\s)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/
+
+  ADDRESS_MAX_SIZE = 50
+  NAME_MAX_SIZE = 50
+  PHONE_MAX_SIZE = 15
+  ZIP_MAX_SIZE = 10
+
+  VALIDATE_ADDRESS = /\A[a-zA-z0-9,\-\s]+\z/
+  VALIDATE_NAME = /\A[a-zA-z\s]+\z/
+  VALIDATE_PHONE = /\A\+[0-9]+\z/
+  VALIDATE_ZIP = /\A[0-9-]+\z/
+
 end
