d06acf75e6e07c0f6ca106dcdca5f084f085c9ba
drkarolina <k.drahanchuk@gmail.com>
2022-10-22 05:40:06 +0300

    Add address controller

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,18 @@
+class AddressesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    address_service = AddressService.new(current_user, address_params)
+    return redirect_to(settings_path, notice: t('.success')) if address_service.call
+
+    @settings_service = SettingsService.new(current_user, address_params, address_service.errors)
+    render 'settings/index'
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:first_name, :last_name, :address, :address_type,
+                                    :city, :zip, :country, :phone)
+  end
+end
