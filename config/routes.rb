ac85317c820c8e404376b17cdf87ae54b11b4968
drkarolina <k.drahanchuk@gmail.com>
2022-10-22 09:58:52 +0300

    Add email and password update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { registrations: 'users/user_data', omniauth_callbacks: 'users/omniauth_callbacks' }
   root "pages#index"
   resources :books, only: %i[index show]
   resources :settings, only: %i[index]
