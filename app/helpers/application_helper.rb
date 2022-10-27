8f5eec025513166c860002203e1bc505dca91949
drkarolina <k.drahanchuk@gmail.com>
2022-10-27 23:38:28 +0300

    Make book`s description support markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def markdown(text)
+    options = {
+      hard_wrap: true, autolink: true, no_intra_emphasis: true, tables: true, fenced_code_blocks: true,
+      disable_indented_code_blocks: true, strikethrough: true, lax_spacing: true, space_after_headers: true,
+      quote: true, footnotes: true, highlight: true, underline: true
+    }
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(text)
+  end
 end
