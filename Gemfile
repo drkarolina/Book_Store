28778b0e94dedff392b89bcebe409040346a743b
drkarolina <k.drahanchuk@gmail.com>
2022-10-27 22:32:37 +0300

    Add and install active admin gem

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.2'
 
+gem 'activeadmin'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bootstrap'
 gem 'country_select', '~> 6.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users, controllers: { registrations: 'users/user_data', omniauth_callbacks: 'users/omniauth_callbacks' }
   root "pages#index"
   resources :books, only: %i[index show]
