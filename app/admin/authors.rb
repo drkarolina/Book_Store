c6ade4658c8825622a70809cd4cb9c61b3b1effc
drkarolina <k.drahanchuk@gmail.com>
2022-10-27 22:41:51 +0300

    Add author to active admin

---
+++ app/admin/authors.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Author do
+  decorate_with AuthorDecorator
+  permit_params :first_name, :last_name, :description
+
+  index do
+    selectable_column
+    id_column
+
+    column :first_name
+    column :last_name
+    column :description
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :first_name
+      row :last_name
+      row :description
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :first_name
+      f.input :last_name
+      f.input :description
+    end
+    actions
+  end
+end
