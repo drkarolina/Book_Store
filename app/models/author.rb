1a0f47f51c40b0b2976be2f30054f2f07e597535
drkarolina <k.drahanchuk@gmail.com>
2022-10-28 04:43:50 +0300

    Update author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
   has_many :author_book, dependent: :destroy
   has_many :books, through: :author_book, dependent: :destroy
+
+  validates :first_name, :last_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_27_192542) do
+ActiveRecord::Schema.define(version: 2022_10_28_013710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2022_10_27_192542) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "books", force: :cascade do |t|
