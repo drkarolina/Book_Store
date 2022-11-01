9f0c83df0ffb424d0ec40fe2ba47b1faa7ff4ee5
drkarolina <k.drahanchuk@gmail.com>
2022-11-02 00:13:05 +0200

    Update active admin reviews

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,16 +1,61 @@
 ActiveAdmin.register Review do
   permit_params :title, :text, :rating, :status, :user_id, :book_id
-  controller { actions :show, :index, :cancel, :edit, :update }
-  form do |f|
-    f.inputs do
-      f.input :title, input_html: { disabled: true }, label: I18n.t('review.field.title')
-      f.input :text, input_html: { disabled: true }, label: I18n.t('review.field.text')
-      f.input :rating, input_html: { disabled: true }, label: I18n.t('review.field.rating')
-      f.input :status, label: I18n.t('review.field.status')
-      f.input :user, as: :select, collection: User.all, input_html: { disabled: true },
-                     label: I18n.t('review.field.user')
-      f.input :book, input_html: { disabled: true }, label: I18n.t('review.field.book')
+  includes :book, :user
+  config.filters = false
+  actions :index, :show
+
+  scope :unprocessed, default: true
+  scope :approved
+  scope :rejected
+
+  controller do
+    private
+
+    def review
+      Review.find(permitted_params[:id])
     end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :book
+    column :title
+    column :created_at
+    column :user
+    column :status
     actions
+  end
+
+  action_item :approve, only: :show do
+    link_to t('links.approve'), approve_admin_review_path(review), method: :put unless review.approved?
+  end
+
+  action_item :reject, only: :show do
+    link_to t('links.reject'), reject_admin_review_path(review), method: :put unless review.rejected?
+  end
+
+  member_action :approve, method: :put do
+    review.update(status: :approved)
+    redirect_to(admin_review_path(review))
+  end
+
+  member_action :reject, method: :put do
+    review.update(status: :rejected)
+    redirect_to(admin_review_path(review))
+  end
+
+  batch_action :approve, if: proc { @current_scope.scope_method != :approved } do |ids|
+    batch_action_collection.find(ids).each do |review|
+      review.update(status: :approved)
+    end
+    redirect_back(fallback_location: admin_reviews_path)
+  end
+
+  batch_action :reject, if: proc { @current_scope.scope_method != :rejected } do |ids|
+    batch_action_collection.find(ids).each do |review|
+      review.update(status: :rejected)
+    end
+    redirect_back(fallback_location: admin_reviews_path)
   end
 end
