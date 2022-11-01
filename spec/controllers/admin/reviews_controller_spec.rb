58fa0f2e79014e96ee76fde7dc5d60c6952fc9fa
drkarolina <k.drahanchuk@gmail.com>
2022-11-02 00:28:17 +0200

    Fix specs

--- spec/controllers/admin/reviews_controller_spec.rb
+++ spec/controllers/admin/reviews_controller_spec.rb
@@ -13,27 +13,21 @@ RSpec.describe Admin::ReviewsController, type: :controller do
 
   context 'when renders the index page' do
     let(:show_path) { send(:admin_review_path, review) }
-    let(:edit_path) { send(:edit_admin_review_path, review) }
 
     before { get :index }
 
     it { expect(page).to have_content('Reviews') }
     it { expect(page).to have_link(review.id, href: show_path) }
-    it { expect(page).to have_link(href: edit_path) }
     it { expect(page).to have_content(review.title) }
-    it { expect(page).to have_content(review.text) }
-    it { expect(page).to have_content(review.rating) }
     it { expect(page).to have_content(review.status) }
     it { expect(page).to have_content(review.user) }
     it { expect(page).to have_content(review.book.title) }
     it { expect(page).to have_content(review.created_at.strftime('%B %d, %Y')) }
-    it { expect(page).to have_content(review.updated_at.strftime('%B %d, %Y')) }
   end
 
   context 'when renders the show page' do
     before { get :show, params: { id: review.to_param } }
 
-    it { expect(page).to have_content('Edit Review') }
     it { expect(page).to have_content(review.title) }
     it { expect(page).to have_content(review.text) }
     it { expect(page).to have_content(review.rating) }
