8126afce72058dbe29644acbd2716513e078ed79
drkarolina <k.drahanchuk@gmail.com>
2022-11-01 22:05:00 +0200

    Add review rating changing with stars logic

--- app/forms/review_form.rb
+++ app/forms/review_form.rb
@@ -3,8 +3,8 @@ class ReviewForm
   attr_accessor :title, :text, :rating, :status, :book_id, :user_id
 
   validates :title, :text, :rating, :book_id, :user_id, presence: true
-  validates :rating, numericality: { greater_than_or_equal_to: Constants::MIN_RATING,
-                                     less_than_or_equal_to: Constants::MIN_RATING }
+  validates :rating, numericality: { greater_than: Constants::MIN_RATING,
+                                     less_than_or_equal_to: Constants::MAX_RATING }
   validates :text, length: { maximum: Constants::MAX_REVIEW_TEXT_LENGTH },
                    format: { with: Constants::VALIDATE_REVIEW,
                              message: I18n.t('books.partials.review_form.invalid_text') }
