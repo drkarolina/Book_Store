cf6a57ad707b95dfc7755d79c13b1b3ef429f054
drkarolina <k.drahanchuk@gmail.com>
2022-11-04 12:34:28 +0200

    Add images to active admin

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,9 @@ class Book < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
 
+  has_one_attached :preview_image
+  has_many_attached :images
+
   validates :title, :price, :published_at, :height,
             :width, :depth, :materials, :quantity, presence: true
 end
