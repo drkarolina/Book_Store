314496289aff5bb4103f7ed420a66b31d693a47b
drkarolina <k.drahanchuk@gmail.com>
2022-11-11 12:29:17 +0200

    Add statuses to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,38 @@
 class Order < ApplicationRecord
+  include AASM
+
   belongs_to :user, optional: true
   has_many :order_items, dependent: :destroy
   has_one :coupon, dependent: :destroy
 
-  enum status: { unprocessed: 0, processed: 1 }
+  enum status: { unprocessed: 0, address: 1, delivery: 2, payment: 3, confirmation: 4, complete: 5 }
+
+  aasm column: :status, enum: true do
+    state :unprocessed, initial: true
+    state :address
+    state :delivery
+    state :payment
+    state :confirmation
+    state :complete
+
+    event :to_address do
+      transitions from: :unprocessed, to: :address
+    end
+
+    event :delivery do
+      transitions from: :address, to: :delivery
+    end
+
+    event :payment do
+      transitions from: :delivery, to: :payment
+    end
+
+    event :confirmation do
+      transitions from: :payment, to: :confirmation
+    end
+
+    event :complete do
+      transitions from: :confirmation, to: :complete
+    end
+  end
 end
