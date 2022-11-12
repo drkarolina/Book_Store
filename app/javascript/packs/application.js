02d5cb615f97512e27dab12aa0477e5da5bf6a84
drkarolina <k.drahanchuk@gmail.com>
2022-11-12 21:10:37 +0200

    Add js to use billing address logic

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require('packs/books/readMore')
 require('packs/books/viewMore')
 require('packs/books/cart')
 require('packs/books/reviewStars')
+require('packs/checkout/useBillingAddress.js')
 require('packs/settings/remove_account')
 import "bootstrap"
 import '@fortawesome/fontawesome-free/css/all'
