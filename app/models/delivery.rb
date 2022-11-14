ddd72ea40912cbe439c1a0fd76d6f47e13c10957
drkarolina <k.drahanchuk@gmail.com>
2022-11-14 03:39:18 +0200

    Add delivery to active admin

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -5,6 +5,6 @@ class Delivery < ApplicationRecord
                                        less_than_or_equal_to: Constants::MAX_DAYS }
   validates :max_days, numericality: { greater_than: :min_days,
                                        less_than_or_equal_to: Constants::MAX_DAYS }
-  validates :price, numericality: { greater_than: Constants::MIN_PRICE }
+  validates :price, numericality: { greater_than_or_equal_to: Constants::MIN_PRICE }
   validates :min_days, :max_days, :price, :method_name, presence: true
 end
