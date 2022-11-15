b2c14caf17c48c788fa7a62bf2cfbc1f5ce673be
drkarolina <k.drahanchuk@gmail.com>
2022-11-15 14:55:39 +0200

    Fix filtering

--- app/services/order_filter_service.rb
+++ app/services/order_filter_service.rb
@@ -5,12 +5,12 @@ class OrderFilterService
   end
 
   def call
-    OrderDecorator.decorate_collection(find_order)
+    OrderDecorator.decorate_collection(find_order.order('updated_at DESC'))
   end
 
   private
 
   def find_order
-    @params ? @user.orders.where(status: @params) : @user.orders.complete
+    @params ? @user.orders.where(status: @params) : @user.orders.where('status >= ?', Order.statuses[:complete])
   end
 end
