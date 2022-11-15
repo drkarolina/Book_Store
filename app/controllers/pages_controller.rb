a8f22a3b79f30295e3eb461b632c35708553cba5
drkarolina <k.drahanchuk@gmail.com>
2022-11-15 14:35:09 +0200

    Move best sellers and newest books  to services

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @newest_books = BookDecorator.decorate_collection(newest_books)
-    @best_sellers = BookDecorator.decorate_collection(best_sellers)
-  end
-
-  private
-
-  def newest_books
-    Book.order('published_at DESC').first(Constants::NUMBER_OF_NEWEST_BOOKS)
-  end
-
-  def best_sellers
-    OrderItem.group(:book).count.sort_by { |_k, v| -v }.map(&:first).first(Constants::NUMBER_OF_BEST_SELLERS)
+    @newest_books = NewestBooksService.call
+    @best_sellers = BestsellerService.call
   end
 end
