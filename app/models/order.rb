92aab4f4520455348f13aa4dabe9ea5795348fe9
drkarolina <k.drahanchuk@gmail.com>
2022-11-15 14:35:09 +0200

    Add order to active admin

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,8 @@ class Order < ApplicationRecord
   has_one :card, dependent: :destroy
   belongs_to :delivery, optional: true
 
-  enum status: { unprocessed: 0, address: 1, delivery: 2, payment: 3, confirmation: 4, complete: 5 }
+  enum status: { unprocessed: 0, address: 1, delivery: 2, payment: 3, confirmation: 4, complete: 5,
+                 in_queue: 6, in_delivery: 7, delivered: 8, canceled: 9 }
 
   aasm column: :status, enum: true do
     state :unprocessed, initial: true
@@ -16,6 +17,10 @@ class Order < ApplicationRecord
     state :payment
     state :confirmation
     state :complete
+    state :in_queue
+    state :in_delivery
+    state :delivered
+    state :canceled
 
     event :to_address do
       transitions from: :unprocessed, to: :address
